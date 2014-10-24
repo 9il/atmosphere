b18ac9340d71563fa1414f5dbed5284aff9c01f0
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2014-10-24 14:26:34 +0400

    update docs

--- source/atmosphere/package.d
+++ source/atmosphere/package.d
@@ -22,15 +22,15 @@ $(DPREF mixture, MixtureOptimizer) $(BLUE abstract)
 
 <table class = "table table-condensed table-bordered">
 <tr><th>Class</th><th>BLAS Level 2 % of all computations</th><th>Non-BLAS parallel</th><th>  Speed (one thread & big data) </th><th>Global maximum for convex function</th><th>Parameterized</th></tr>
-<tr><td>GradientDescent, k* > ~16</td><td>~95-99%</td><td>No</td><td> normal </td><td>Yes</td><td>No</td></tr>
-<tr><td>GradientDescent, k* < ~16</td><td>~95-99%</td><td>No</td><td> fast </td><td>Yes</td><td>No</td></tr>
+<tr><td>GradientDescent, number of mixture components > ~16</td><td>~95-99%</td><td>No</td><td> normal </td><td>Yes</td><td>No</td></tr>
+<tr><td>GradientDescent, number of mixture components < ~16</td><td>~95-99%</td><td>No</td><td> fast </td><td>Yes</td><td>No</td></tr>
 <tr><td>CoordinateDescent</td><td>~10-50%</td><td>No</td><td> fast </td><td>Yes</td><td>No</td></tr>
 <tr><td>CoordinateDescentPartial</td><td>~10-50%</td><td>No</td><td> fast, always faster then CoordinateDescent </td><td>Yes</td><td>No</td></tr>
 <tr><td>NormalVarianceMeanMixtureEM</td><td>~10%</td><td>Yes</td><td> slow </td><td>No</td><td>Yes</td></tr>
 <tr><td>NormalVarianceMeanMixtureEMAndGradient</td><td>~10%</td><td>Yes</td><td> slow </td><td>No</td><td>Yes</td></tr>
 <tr><td>NormalVarianceMeanMixtureEMAndCoordinate</td><td>~5%</td><td>Partial</td><td> slow </td><td>No</td><td>Yes</td></tr>
 </table>
-\* - where k is a number of mixture components.
+
 Example: 
 	$(HTTP github.com/9il/atmosphere_gm/blob/master/examples/normal_variance_mean_mixture/source/app.d, GitHub) 
 
