4aee4b443d43710d07e194fc87e30c55df416f50
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2014-10-25 22:02:16 +0400

    update test

--- c/catmosphere.c
+++ c/catmosphere.c
@@ -11,7 +11,7 @@ int main(int argc, char const *argv[])
 	// needs to call rt_init!
 	
 	double alpha1 = catmosphere_nvmm_em_and_coordinate(0,0,NULL,NULL,NULL,NULL,NULL);
-	double alpha2 =catmosphere_nvmm_em_and_gradient(0,0,NULL,NULL,NULL,NULL,NULL);
-	double alpha3 =catmosphere_nvmm_em(0,0,NULL,NULL,NULL,NULL,NULL);
+	double alpha2 = catmosphere_nvmm_em_and_gradient(0,0,NULL,NULL,NULL,NULL,NULL);
+	double alpha3 = catmosphere_nvmm_em(0,0,NULL,NULL,NULL,NULL,NULL);
 	return 0;
 }
