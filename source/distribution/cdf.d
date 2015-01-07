d3b6b5015f650aae45d48d1b2ee6cff722869986
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-01-07 10:36:48 +0600

    GeneralizedGammaCDF

--- source/distribution/cdf.d
+++ source/distribution/cdf.d
@@ -60,7 +60,7 @@ final class GammaCDF(T) : CDF!T
 
 	T opCall(T x)
 	{
-		return x < 0 ? 0 : gammaIncomplete(shape, x / scale);
+		return x <= 0 ? 0 : gammaIncomplete(shape, x / scale);
 	}
 }
 
@@ -96,7 +96,7 @@ final class InverseGammaCDF(T) : CDF!T
 
 	T opCall(T x)
 	{
-		return x < 0 ? 0 : gammaIncomplete(shape, scale / x);
+		return x <= 0 ? 0 : gammaIncomplete(shape, scale / x);
 	}
 }
 
@@ -104,6 +104,52 @@ final class InverseGammaCDF(T) : CDF!T
 unittest 
 {
 	auto cdf = new InverseGammaCDF!double(3, 2);
+	auto x = cdf(0.1);
+	assert(isNormal(x));
+}
+
+
+/++
+Generalized gamma CDF
++/
+final class GeneralizedGammaCDF(T) : CDF!T
+	if(isFloatingPoint!T)
+{
+	private T shape, power, scale, gammaShape;
+
+	/++
+	Constructor
+	Params:
+		shape = shape parameter
+		power = power parameter
+		scale = scale parameter
+	+/
+	this(T shape, T power, T scale = 1)
+	in {
+		assert(shape.isNormal);
+		assert(shape > 0);
+		assert(power.isFinite);
+		assert(scale.isNormal);
+		assert(scale > 0);
+	}
+	body {
+		this.shape = shape;
+		this.power = power;
+		this.scale = scale;
+		this.gammaShape = gamma(shape);
+		assert(gammaShape.isNormal);
+	}
+
+	T opCall(T x)
+	{
+		return x <= 0 ? 0 : gammaIncomplete(shape, pow(x / scale, power)) / gammaShape;
+	}
+}
+
+///
+unittest 
+{
+	auto cdf = new GeneralizedGammaCDF!double(3, 2, 0.5);
 	auto x = cdf(0.1);
 	assert(isNormal(x));
 }
