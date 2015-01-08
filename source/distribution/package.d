cd1e6df3fcfe93a0583e8d51872b900f06f8e8fa
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-01-08 12:15:29 +0600

    remove comments in distribution package

--- source/distribution/package.d
+++ source/distribution/package.d
@@ -1,19 +1,8 @@
-/**
-Publicly imports 
-$(LINK2 distribution/random.html, random),
-$(LINK2 distribution/pdf.html, pdf),
-$(LINK2 distribution/cdf.html, cdf),
-$(LINK2 distribution/quantile.html, quantile).
-*/
+///
 module distribution;
 
-///
 public import distribution.cdf;
-///
 public import distribution.pdf;
-///
 public import distribution.quantile;
-///
 public import distribution.random;
-///
 public import distribution.derivative;
