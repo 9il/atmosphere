34c4377f4bf8900d1dbeb560cb5c6cf46a49c736
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-02-02 01:51:36 +0300

    update distribution package

--- source/distribution/package.d
+++ source/distribution/package.d
@@ -2,9 +2,11 @@
 module distribution;
 
 public import distribution.cdf;
+public import distribution.derivative;
+public import distribution.finitemixture;
+public import distribution.moment;
+public import distribution.params;
 public import distribution.pdf;
 public import distribution.quantile;
 public import distribution.random;
-public import distribution.derivative;
-public import distribution.params;
-public import distribution.moment;
+public import distribution.utilities;
