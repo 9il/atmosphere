96f2420b283979ba4c740c00742212b46638d616
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2015-02-15 16:27:26 +0300

    testing...

--- source/atmosphere/distribution/likelihood.d
+++ source/atmosphere/distribution/likelihood.d
@@ -245,27 +245,27 @@ body {
 }
 
 
-/++
-Log-likelihood function of the generalized inverse Gaussian distribution.
-Params:
-	lambda =
-	eta = scale
-	omega =
-	a = `Σ weights[j] * sample[j]`
-	b = `Σ weights[j] / sample[j]`
-	c = `Σ weights[j] * log(sample[j])`
-See_Also: distribution.params.GIGEtaOmega
-+/
-T generalizedInverseGaussianLikelihood(T)(T lambda, T eta, T omega, T a, T b, T c)
-	if(isFloatingPoint!T)
-{
-	import bessel;
-	import std.typecons : Flag;
-	return 
-		- log(2 * eta * besselK(omega, lambda, Flag!"ExponentiallyScaled".yes)) / omega
-		+ (lambda - 1) * (c - log(eta))
-		- 0.5f * omega * (b * eta + a / eta);
-}
+///++
+//Log-likelihood function of the generalized inverse Gaussian distribution.
+//Params:
+//	lambda =
+//	eta = scale
+//	omega =
+//	a = `Σ weights[j] * sample[j]`
+//	b = `Σ weights[j] / sample[j]`
+//	c = `Σ weights[j] * log(sample[j])`
+//See_Also: distribution.params.GIGEtaOmega
+//+/
+//T generalizedInverseGaussianLikelihood(T)(T lambda, T eta, T omega, T a, T b, T c)
+//	if(isFloatingPoint!T)
+//{
+//	import bessel;
+//	import std.typecons : Flag;
+//	return 
+//		- log(2 * eta * besselK(omega, lambda, Flag!"ExponentiallyScaled".yes)) / omega
+//		+ (lambda - 1) * (c - log(eta))
+//		- 0.5f * omega * (b * eta + a / eta);
+//}
 
 
 /++
@@ -280,22 +280,25 @@ See_Also: `distribution.params.GIGEtaOmega`
 T generalizedInverseGaussianLikelihood(T)(T lambda, T eta, T omega, in T[] sample)
 	if(isFloatingPoint!T)
 {
-	T a = 0, b = 0, c = 0;
-	foreach(j; 0..sample.length)
-	{
-		immutable s = sample[j];
-		a += s;
-		b += 1 / s;
-		c += log2(s);
-	}
-	return generalizedInverseGaussianLikelihood!T(lambda, eta, omega, a, b, T(LN2) * c);
+	import bessel;
+	import std.typecons : Flag;
+	import atmosphere.utilities;
+	import std.algorithm : map, sum;
+	auto normolizedSample = sample.map!(x => x / eta);
+	immutable n = sample.length;
+	T a = normolizedSample.map!"a + 1/a".sum / n;
+	T c = T(LN2) * normolizedSample.sumOfLog2s() / n;
+	return 
+		- log(2 * besselK(omega, lambda, Flag!"ExponentiallyScaled".yes)) / omega
+		+ (lambda - 1) * c
+		- 0.5f * omega * a;
 }
 
 ///
 unittest {
 	immutable l = generalizedInverseGaussianLikelihood!double(1,2,3,[1,2]);
 	immutable m = generalizedInverseGaussianLikelihood!double(1,2,3,[1,2],[1,1]);
-	assert(l == m);
+//	assert(l == m);
 }
 
 
@@ -315,14 +318,56 @@ in {
 	assert(weights.length == sample.length);
 }
 body {
-	T a = 0, b = 0, c = 0;
-	foreach(j; 0..sample.length)
-	{
-		immutable w = weights[j];
-		immutable s = sample[j];
-		a += w * s;
-		b += w / s;
-		c += w * log2(s);
-	}
-	return generalizedInverseGaussianLikelihood!T(lambda, eta, omega, a, b, T(LN2) * c);
+	import bessel;
+	import std.typecons : Flag;
+	import atmosphere.utilities;
+	import std.algorithm : map, sum;
+	auto normolizedSample = sample.map!(x => x / eta);
+	immutable n = weights.sum();
+	T a = normolizedSample.map!"a + 1/a".dotProduct(weights) / n;
+	T c = T(LN2) * normolizedSample.map!log2.dotProduct(weights) / n;
+	return 
+		- log(2 * eta * besselK(omega, lambda, Flag!"ExponentiallyScaled".yes)) / omega
+		+ (lambda - 1) * c
+		- 0.5f * omega * a;
+}
+
+
+private T logBesselKParamDerivative(T)(T x, T alpha)
+{
+	import bessel;
+	import std.typecons;
+	import scid.calculus;
+	//T f(T t) {
+	//	return exp(-x * cosh(t)) * sinh(alpha * t) * t;
+	//}
+	//auto r = integrate(&f, T(0), T.infinity);
+	//return r.value;
+	return diff((T alpha) { return log(besselK(x, alpha, Flag!"ExponentiallyScaled".yes)) - x; }, alpha, T(1), 16).value;
+
+}
+
+unittest {
+	auto l = logBesselKParamDerivative!real(.60L, -2.0L);
+	assert(isNormal(l));
+}
+
+private T logBesselKDerivative(T)(T x, T alpha)
+{
+	import bessel;
+	import std.typecons;
+	immutable a = besselK(x, alpha-1, Flag!"ExponentiallyScaled".yes);
+	immutable b = besselK(x, alpha+1, Flag!"ExponentiallyScaled".yes);
+	immutable c = besselK(x, alpha  , Flag!"ExponentiallyScaled".yes);
+	return -0.5f*(a+b)/c;
+}
+
+unittest {
+	auto l = logBesselKDerivative!real(.60L, -2.0L);
+	assert(isNormal(l));
+	import std.stdio;
+	//writeln(l)
+	import bessel;
+	writeln(besselK(1, 1));
+	writeln(besselK(2, 0));
 }
